21b469472c98908e94114c85f2d504f91b210c9f
devincornell <devincornell@example.com>
2021-12-01 14:57:30 -0500

    added command line features

--- objstore/__main__.py
+++ objstore/__main__.py
@@ -1,15 +1,18 @@
-
+import uvicorn
+import argparse
+from .server import app
 
 if __name__ == '__main__':
-
-    import argparse
+    
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', help='Port to listen on.')
 
-
+    # just copied from these docs:
+    # https://www.uvicorn.org/#running-programmatically
+    parser.add_argument('-p', '--port', help='Port to listen on.', type=int, default=8000)
+    parser.add_argument('--host', help='Hostname to listen on.', type=str, default='localhost')
+    parser.add_argument('-l', '--log_level', help='Log level.', type=str, default='info')
+    #parser.add_argument('-r', '--reload', help='Reload when server code is changed?', action='store_true')
     args = parser.parse_args()
-    print(args)
-    print(args.hello)
 
-    #print('hello world', __name__)
+    uvicorn.run(app, **vars(args))
 
