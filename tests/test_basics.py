478ad8b6d68a7f01be1c63aa2d8a0b4bfa4c6bf1
Devin J. Cornell <devincornell@github.com>
2023-11-17 13:08:52 -0500

    changing print statements to assertions

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,23 +8,24 @@ from requests.models import HTTPError
 sys.path.append('..')
 import objstore
 
-PORT = 8001
+PORT = 8000
 HOST = 'localhost'
 
 
 def test_basics():
     
-    
     client = objstore.Client(HOST, PORT)
-    assert(client.status()['is_alive'])
+    status = client.status()
+    assert(status['is_alive'])
     
-    
-    print(client.status())
-    print(client.list_repos())
+    assert(status['num_repos'] == 0)
+    assert(status['num_items'] == 0)
+    assert(client.list_repos() == [])
 
     repo = client.get_repo('new_repo')
-    print(client.list_repos())
-    print(repo.list_keys())
+    assert(len(client.list_repos()) == 1)
+    assert(repo.list_keys() == [])
+    
     print(repo.put_data('a', range(10)))
     print(list(repo.get_data('a')))
     print(repo.list_keys())
