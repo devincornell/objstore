df337cc87c255826df5cb0588a637a9dffa00559
Devin J. Cornell <devincornell@github.com>
2023-11-17 14:22:58 -0500

    fixed import issue

--- objstore/__init__.py
+++ objstore/__init__.py
@@ -1,6 +1,6 @@
 
 from .client import Client, Repository
-from .server import app
+from .server import app, run_server
 from .errors import *
 from .run import run
 #from .repository import Repository
