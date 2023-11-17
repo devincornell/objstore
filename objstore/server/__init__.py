21b068b6743897a118739c3455ba8c719a8e5417
Devin J. Cornell <devincornell@github.com>
2023-11-17 13:46:13 -0500

    exposed run_server command

--- objstore/server/__init__.py
+++ objstore/server/__init__.py
@@ -3,3 +3,19 @@
 from .server import app
 from .repodata import RepoData
 #from .server import app
+
+import uvicorn
+def run_server(
+    host: str = 'localhost', 
+    port: int = 8000, 
+    log_level: str = 'info'
+) -> None:
+    '''Run the uvicorn server.'''
+    return uvicorn.run(
+        app, 
+        host=host,
+        port=port,
+        log_level=log_level,
+    )
+
+
