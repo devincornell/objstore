303daa977ee103c91eec6fe0a06c29ceb4cffd31
Devin J. Cornell <devincornell@github.com>
2023-11-17 13:08:38 -0500

    created accessor for request data

--- objstore/server/server.py
+++ objstore/server/server.py
@@ -7,9 +7,12 @@ import io
 from ..errors import RepoDoesNotExist, KeyDoesNotExist
 from .repodata import RepoData
 
+def data_from_request(request: fastapi.Request) -> RepoData:
+    '''Return repodata from app state.
+    '''
+    return request.app.state.repodata
+
 app = fastapi.FastAPI()
-
-
 
 @app.on_event("startup")
 async def startup():
@@ -22,24 +25,24 @@ async def startup():
 async def status(request: fastapi.Request):
     '''Get the status of the server.
     '''
+    rdata = data_from_request(request)
     return {
         "is_alive": True, 
-        'num_repos': len(request.app.state.repodata),
-        'num_items': request.app.state.repodata.num_items(),
+        'num_repos': len(rdata),
+        'num_items': rdata.num_items(),
     }
 
 @app.get("/repositories/list")
 async def list_repos(request: fastapi.Request):
     '''List repositories on the server.
     '''
-    return list(request.app.state.repodata.list_repos())
+    return data_from_request(request).list_repos()
 
 @app.post("/repositories/new", status_code=201)
 async def make_repo(request: fastapi.Request, repo_name: str):
     '''Make a new repository.
     '''
-    repodata = request.app.state.repodata
-    repodata.make_repo(repo_name)
+    data_from_request(request).make_repo(repo_name)
 
 
 ############## manage repositories ##############
@@ -48,13 +51,13 @@ async def make_repo(request: fastapi.Request, repo_name: str):
 async def get_repo_keys(request: fastapi.Request, repo_name: str):
     '''Get list of keys in the repository.
     '''
-    return list(request.app.state.repodata.get_data(repo_name).keys())
+    return list(data_from_request(request).get_data(repo_name).keys())
 
 @app.get("/repositories/repo/{repo_name}/delete")
 async def delete_repo(request: fastapi.Request, repo_name: str):
     '''Delete the given repository.
     '''
-    request.app.state.repodata.delete_repo(repo_name)
+    data_from_request(request).delete_repo(repo_name)
     return {'success': True}
 
 
@@ -64,19 +67,19 @@ async def delete_repo(request: fastapi.Request, repo_name: str):
 async def get_data(request: fastapi.Request, repo_name: str, key: Optional[str] = None):
     '''Get all (when key is None) or specific data from an existing repository.
     '''
-    data = pickle.dumps(request.app.state.repodata.get_data(repo_name, key=key))
+    data = pickle.dumps(data_from_request(request).get_data(repo_name, key=key))
     return fastapi.responses.StreamingResponse(io.BytesIO(data))
 
 @app.put("/repositories/repo/{repo_name}/data", status_code=201)
 async def put_data(request: fastapi.Request, repo_name: str, key: str, file: fastapi.UploadFile = fastapi.File(None)):
     ''''Store data in a repository.
     '''
-    request.app.state.repodata.put_data(repo_name, key, pickle.load(file.file))
+    data_from_request(request).put_data(repo_name, key, pickle.load(file.file))
     return {'success': True}
 
 @app.delete("/repositories/repo/{repo_name}/data")
 async def delete_data(request: fastapi.Request, repo_name: str, key: str):
     '''Delete repository or specific data.
     '''
-    request.app.state.repodata.delete_data(repo_name, key)
+    data_from_request(request).delete_data(repo_name, key)
     return {'success': True}
